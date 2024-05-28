2923c45d82a587f72e69d1dd22c88fe8d6a5a7d0
Rohaid <rohaidb@protonmail.com>
2024-05-27 21:26:36 -0400

    backup

---
+++ ro-VideoGameCatalogue/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ro_VideoGameCatalogue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
